aef78576386c7a4d87bd4271f605468b13bafcfb
DiegoDulval <diegodifreitas@gmail.com>
2018-04-27 14:54:24 -0300

    generated a service account

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,5 +1,12 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
+const serviceAccount = require("./service_account.json");
 const createUser = require("./create_user");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-react.firebaseio.com"
+});
 
 exports.createUser = functions.https.onRequest(createUser);
