e41c8d5c4cc181093ba99171df580da685285ba4
DiegoDulval <diegodifreitas@gmail.com>
2018-05-03 10:49:30 -0300

    Create function for verify one time password

--- one-time-password/functions/verify_one_time_password.js
+++ one-time-password/functions/verify_one_time_password.js
@@ -1,3 +1,30 @@
-module.exports = funtion(req, res){
-    
-}
+const admin = require("firebase-admin");
+
+module.exports = function(req, res) {
+  if (!req.body.phone || !re.body.code) {
+    return res.status(422).send({ error: "Phone and code must   be provided" });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+  const code = parseInt(code);
+
+  admin
+    .auth()
+    .getUser(phone)
+    .then(() => {
+      const ref = admin.database().ref("users/" + phone);
+
+      ref.on("value", snapshot => {
+        const user = snapshot.val();
+
+        //compare codes
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: "Code not valid" });
+        }
+
+        //mark codeValid is false
+        ref.update({ codeValid: false });
+      });
+    })
+    .catch(err => res.status(422).send({ error: err }));
+};
