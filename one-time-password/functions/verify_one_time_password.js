935d280617c593483ac0766c83795f99b7e70e0c
DiegoDulval <diegodifreitas@gmail.com>
2018-05-03 11:23:39 -0300

    updated verify one time password

--- one-time-password/functions/verify_one_time_password.js
+++ one-time-password/functions/verify_one_time_password.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
 
 module.exports = function(req, res) {
-  if (!req.body.phone || !re.body.code) {
-    return res.status(422).send({ error: "Phone and code must   be provided" });
+  if (!req.body.phone || !req.body.code) {
+    return res.status(422).send({ error: "Phone and code must be provided" });
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
