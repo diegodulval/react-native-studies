4440f43ec70a811fb20b2c92573baee33704a203
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-24 14:10:36 -0300

    Create component SingIn

---
+++ firebase-auth/components/SignInForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { FormLabel, FormInput, Button } from "react-native-elements";
+import axios from "axios";
+
+const ROOT_URL = "https://us-central1-one-time-react.cloudfunctions.net/";
+
+class SignInForm extends Component {
+  state = { phone: "", code: "" };
+
+  handleSubmit = async () => {
+    const user = { phone: this.state.phone, code: this.state.code };
+    try {
+      let { data } = await axios.post(`${ROOT_URL}/verifyOneTimePassword`, user);
+    } catch (err) {
+      console.log("Deu erro!");
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Code</FormLabel>
+          <FormInput
+            value={this.state.code}
+            onChangeText={code => this.setState({ code })}
+          />
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit" />
+      </View>
+    );
+  }
+}
+
+export default SignInForm;
