46d5e543f4d42a0b2c03387d7e97247e073eb6d9
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-25 16:40:09 -0300

    Created screen boilerplate

---
+++ jobs/screens/WelconeScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class WelconeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WelconeScreen</Text>
+        <Text>WelconeScreen</Text>
+        <Text>WelconeScreen</Text>
+        <Text>WelconeScreen</Text>
+        <Text>WelconeScreen</Text>
+        <Text>WelconeScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default WelconeScreen;
