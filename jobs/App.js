efd98347d3ad4d821e11257452b57f1cc4f26f50
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-25 17:50:30 -0300

    Create StackNavigation on review tab

--- jobs/App.js
+++ jobs/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
 
 import AuthScreen from "./screens/AuthScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import MapScreen from "./screens/MapScreen";
 import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 class App extends React.Component {
   render() {
@@ -15,7 +20,13 @@ class App extends React.Component {
       main: {
         screen: createBottomTabNavigator({
           map: { screen: MapScreen },
-          deck: { screen: DeckScreen }
+          deck: { screen: DeckScreen },
+          review: {
+            screen: createStackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
         })
       }
     });
