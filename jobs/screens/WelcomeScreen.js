ebb0f65ca4a49b9394e7d6c05129146f150d48fe
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-28 18:48:53 -0300

    Added data slide

--- jobs/screens/WelcomeScreen.js
+++ jobs/screens/WelcomeScreen.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+import Slides from "../components/Slides";
+
+const SLIDE_DATA = [
+  { text: "Welcome to JobApp" },
+  { text: "Set yout location, then swipe away" },
+  { text: "Job Finder will help you get a job!" }
+];
+
 class WelcomeScreen extends Component {
   render() {
-    return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
-    );
+    return <Slides data={SLIDE_DATA} />;
   }
 }
 
