334c506f6d6f0ab0c901962e728b8dd8f2409137
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-29 11:29:36 -0300

    Created Button on last slide

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 import { View, Text, ScrollView, Dimensions } from "react-native";
+import { Button } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          buttonStyle={styles.buttonStyle}
+          title="Onwards!"
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
           key={slide.id}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });
@@ -37,6 +50,10 @@ const styles = {
     color: "white",
     fontSize: 30,
     textAlign: "center"
+  },
+  buttonStyle: {
+    backgroundColor: "#0288D1",
+    marginTop: 15
   }
 };
 
