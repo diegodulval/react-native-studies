2ed152ee0bcc83648879da0d3e296a059140e912
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-29 11:08:52 -0300

    Styling slide component

--- jobs/components/Slides.js
+++ jobs/components/Slides.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 class Slides extends Component {
+  renderSlides() {
+    return this.props.data.map(slide => {
+      return (
+        <View
+          key={slide.id}
+          style={[styles.slideStyle, { backgroundColor: slide.color }]}
+        >
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Slides</Text>
-        <Text>Slides</Text>
-        <Text>Slides</Text>
-        <Text>Slides</Text>
-        <Text>Slides</Text>
-      </View>
+      <ScrollView horizontal pagingEnabled style={{ flex: 1 }}>
+        {this.renderSlides()}
+      </ScrollView>
     );
   }
 }
+
+const styles = {
+  slideStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: SCREEN_WIDTH
+  },
+  textStyle: {
+    color: "white",
+    fontSize: 30,
+    textAlign: "center"
+  }
+};
 
 export default Slides;
