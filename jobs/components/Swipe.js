768a99f2c8da9f4da328de55600708feaec8ad1e
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-30 19:13:22 -0300

    resolve issue of key

--- jobs/components/Swipe.js
+++ jobs/components/Swipe.js
@@ -15,7 +15,8 @@ const SWIPE_OUT_DURATION = 250;
 class Swipe extends Component {
   static defaultProps = {
     onSwipeRight: () => {},
-    onSwipeLeft: () => {}
+    onSwipeLeft: () => {},
+    keyProp: "id"
   };
 
   constructor(props) {
@@ -101,7 +102,7 @@ class Swipe extends Component {
       if (i === this.state.index) {
         return (
           <Animated.View
-            key={item.id}
+            key={item[this.props.keyProp]}
             style={[this.getCardStyle(), styles.cardStyle, { zIndex: 99 }]}
             {...this.state.panResponder.panHandlers}
           >
@@ -112,7 +113,7 @@ class Swipe extends Component {
 
       return (
         <Animated.View
-          key={item.id}
+          key={item[this.props.keyProp]}
           style={[
             styles.cardStyle,
             {
